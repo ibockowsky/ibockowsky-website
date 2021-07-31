6c8e47ea2d9c402cd74acc67376e162d4d128544
ibockowsky <igor@newfantastic.com>
2021-07-31 15:31:28 +0200

    chore: initial project setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+  <slot></slot>
+</div>
+
+<style lang="scss">
+@import '../../static/styles/normalize.css';  
+.container {
+  @include flex($align-items: center, $justify-content: center);
+  @include size($x: 100vw, $y: 100vh);
+}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 /** @type {import('@sveltejs/kit').Config} */
+import sveltePreprocess from 'svelte-preprocess'
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
-};
+	},
+	preprocess: sveltePreprocess({
+		scss: {
+			includePaths: ['src', 'node_modules'],
+			prependData: `@import 'sxcss';`
+		}
+	})
+}
 
-export default config;
+export default config
