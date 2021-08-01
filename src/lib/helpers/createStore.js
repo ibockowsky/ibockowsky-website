f878a540c499d2423aa76e03706afae86ddc9446
ibockowsky <igor@newfantastic.com>
2021-08-01 19:51:24 +0200

    refactor: code clean up

--- src/lib/helpers/createStore.js
+++ src/lib/helpers/createStore.js
@@ -3,16 +3,19 @@ import { writable } from 'svelte/store'
 export const createStore = (store) => {
 	const { set, update: nativeUpdate, subscribe } = writable(store)
 
-	const update = (argOne, argTwo) => {
+	const update = (argOne, argTwo = undefined) => {
 		if (Array.isArray(argOne) && Array.isArray(argTwo)) {
-			if (argOne.length !== argTwo.length) console.error(`Arrays length aren't equal.`)
+			if (argOne.length !== argTwo.length) {
+				console.error(`Arrays length aren't equal.`)
+				return
+			}
 
 			let entries = {}
 			for (let i = 0; i < argOne.length; i++) {
 				entries[argOne[i]] = argTwo[i]
 			}
-			nativeUpdate((state) => ({ ...state, ...entries }))
-			return
+
+			return nativeUpdate((state) => ({ ...state, ...entries }))
 		}
 
 		if (typeof argOne === 'object') {
@@ -20,11 +23,11 @@ export const createStore = (store) => {
 			for (const property in argOne) {
 				entries[property] = argOne[property]
 			}
-			nativeUpdate((state) => ({ ...state, ...entries }))
-			return
+
+			return nativeUpdate((state) => ({ ...state, ...entries }))
 		}
 
-		nativeUpdate((state) => ({ ...state, [argOne]: argTwo }))
+		return nativeUpdate((state) => ({ ...state, [argOne]: argTwo }))
 	}
 
 	return {
